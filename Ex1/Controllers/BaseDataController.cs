435f9eb5004bc0b4f0a9b2d3c1c57c1fec5c26a1
NikolaiB <altnikolos95@gmail.com>
2023-01-29 23:59:28 +0300

    Add project files.

---
+++ Ex1/Controllers/BaseDataController.cs
@@ -0,0 +1,38 @@
+﻿using Ex1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Ex1.Controllers
+{
+    public class BaseDataController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public BaseDataController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult OutputData()
+        {
+            var model = new OutputData();
+            model.name = "Nik";
+            model.date = DateTime.Now.ToString();
+            model.applicationName = _webHostEnvironment.ApplicationName;
+            model.environmentName = _webHostEnvironment.EnvironmentName;
+            model.host = HttpContext.Request.Host.Value;
+            model.protocol = HttpContext.Request.Protocol;
+            return View(model);
+        }
+
+        //public IActionResult SecondTask()
+        //{
+        //    var obj = new ObjectController();
+        //    var jsonOptions = new JsonSerializerOptions
+        //    {
+        //        PropertyNameCaseInsensitive = true,
+        //        WriteIndented = true
+        //    };
+        //    ViewBag.json = JsonSerializer.Serialize(obj, jsonOptions);
+        //    return View();
+        //}
+    }
+}
---
+++ Ex1/Models/OutputData.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex1.Models
+{
+    public class OutputData
+    {
+        public string? name;
+        public string? date;
+        public string? environmentName;
+        public string? applicationName;
+        public string? host;
+        public string? protocol;
+    }
+}
